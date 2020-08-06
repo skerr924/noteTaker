774d5d85feae2d160ce7cab61b74a8795943dbfa
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-06 16:39:03 -0400

    all get, post, and delete routes added

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@
 // ===========================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs"); 
 
 // Sets up the Express App
 // ===========================================
@@ -12,7 +13,7 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//jQuery calls to various HTML elements 
 const $noteTitle = $(".note-title");
 const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
@@ -21,6 +22,81 @@ const $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
+
+// Routes
+// =============================================================
+
+//displays homepage 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//displays notes page 
+app.get("/notes/", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+//gets all notes from the db.json file 
+app.get('/api/notes', function(req, res) {
+
+  try { 
+  const storedNotes = fs.readFileSync("../../db/db.json", "utf8"); 
+  storedNotes = JSON.parse(storedNotes); 
+  } catch (err) { 
+    console.log ("error getting api stored notes"); 
+  }
+
+  res.json(storedNotes); 
+
+});
+
+app.post("/api/notes", function(req, res) {
+  try { 
+    const storedNotes = fs.readFileSync("../../db/db.json", "utf8"); 
+    storedNotes = JSON.parse(storedNotes); 
+    req.body.id = storedNotes.length; 
+    storedNotes.push(req.body)
+    storedNotes = JSON.stringify(storedNotes); 
+
+    try {
+    fs.writeFile("../../db/db.json", storedNotes, "utf8")
+    } 
+    catch (err) { 
+      console.log ("error writing to api"); 
+    }
+  
+    res.json(JSON.parse(storedNotes));
+  }  catch (err) { 
+      console.log ("error writing to the API"); 
+  }
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  try { 
+    const storedNotes = fs.readFileSync("../../db/db.json", "utf8"); 
+    storedNotes = JSON.parse(storedNotes); 
+
+    //filters through the notes and returns all notes that don't have the same 
+    // id as the specNote we are trying to remove 
+    storedNotes = storedNotes.filter(function(specNote){ 
+      return specNote.id != req.params.id; 
+    }); 
+
+    storedNotes = JSON.stringify(storedNotes); 
+    fs.writeFile("../../db/db.json", storedNotes, "utf8", function(err){ 
+      if (err){ 
+        throw err;
+      }
+    }); 
+
+    res.send(JSON,parse(storedNotes)); 
+  
+  } catch (err) { 
+    console.log("error writing to the API"); 
+  }
+
+}); 
+
 
 // A function for getting all notes from the db
 const getNotes = () => {
